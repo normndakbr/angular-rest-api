a63e338475df275d2c14841ea13adc034ae0d438
normndakbr <normnd.akbr2@gmail.com>
2021-05-03 11:34:25 +0700

    refactor article model

--- server/models/articleModel.js
+++ server/models/articleModel.js
@@ -1,13 +1,17 @@
 module.exports = mongoose => {
-    const Article = mongoose.model(
-        "article",
-        mongoose.Schema({
-            title: String,
-            description: String,
-            writer: String,
-            published: Boolean
-        }, { timestamps: true })
-    );
+    var schema = mongoose.Schema({
+        title: String,
+        description: String,
+        writer: String,
+        published: Boolean
+    }, { timestamps: true });
 
+    schema.method("toJSON", () => {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    })
+
+    const Article = mongoose.model("tutorial", schema);
     return Article;
 }
