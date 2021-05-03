78be80114bfd51c7cda190e634f1036f58dab2d6
normndakbr <normnd.akbr2@gmail.com>
2021-05-03 11:10:43 +0700

    applied database to the server

--- server/app.js
+++ server/app.js
@@ -3,10 +3,24 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const db = require('./models')
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connected successfully!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to database!", err);
+    })
+
 app.listen(PORT, () => {
     console.log(`Application is listening on http://localhost:${PORT}`);
 })
